dba9be5456ac4fce794b766c70587ca7669799ba
Sabari Krishna V K <sabari@Sabaris-MacBook-Pro.local>
2020-12-20 21:25:43 -0500

    product details

--- setup/dxm.sql
+++ setup/dxm.sql
@@ -224,14 +224,14 @@ INSERT INTO sitepages (sitepagesid,sitesid,pagetemplatesid,pagename,pageurl,"ver
 (gen_random_uuid(), (select sitesid from sites where sitename ='Galaxy'), (select pagetemplatesid from pagetemplates where filename='pdppage'),
 'Galaxy PDP Page','products/keyId',1,'Active','2020-12-01','2021-12-31',
 '{"header": "header1","footer":"footer1","body":"body2"}',
-'{"menu": "menu-horizontal","row1column0": {"content":"pdp-banner"},"row2column1": {"component":"product-card"} ,"row2column2": {"component":"add-to-cart"}, "row3column0": {"component":"product-review"} }', 
+'{"menu": "menu-horizontal","row1column0": {"content":"pdp-banner"},"row2column1": {"component":"product-details", "sub-components":[{"col1row0":"product-image","col2row1":"product-info","col2row2":"add-to-cart"}] } ,"row2column2": {"component":"cart-summary"}, "row3column0": {"component":"related-items"} }', 
 '{"description": "PDP Page", "title":"Welcome to Galaxy.com"}' ) ;
 
 INSERT INTO sitepages (sitepagesid,sitesid,pagetemplatesid,pagename,pageurl,"version",status,startdate,enddate, widgets,components,metadata) VALUES 
 (gen_random_uuid(), (select sitesid from sites where sitename ='Galaxy'), (select pagetemplatesid from pagetemplates where filename='cartpage'),
 'Galaxy Cart Page','cart/keyId',1,'Active','2020-12-01','2021-12-31',
 '{"header": "header1","footer":"footer1","body":"body2"}',
-'{"menu": "menu-horizontal","row1column0": {"content":"cart-banner"},"row2column1": {"component":"cart-items"} ,"row2column2": {"component":"cart-summary"}, "row3column0": {"component":"cart-item-suggestions"} }', 
+'{"menu": "menu-horizontal","row1column0": {"content":"cart-banner"},"row2column1": {"component":"cart-items"} ,"row2column2": {"component":"cart-summary"}, "row3column0": {"component":"related-items"} }', 
 '{"description": "Cart Page", "title":"Welcome to Galaxy.com" }' ) ;
  
 
